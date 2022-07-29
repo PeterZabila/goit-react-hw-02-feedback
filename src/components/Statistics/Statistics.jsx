ad1d3c1e99d82a5d3f7d1178a7f3426da96b77b7
ZABILA PETRO <petrozabila@gmail.com>
2022-07-29 09:08:40 +0300

    1 task before refactor

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -9,6 +9,28 @@ class Statistics extends Component {
         neutral: 0,
         bad: 0
       }
+
+
+onLeaveFeedback = (event) => {
+    console.log(this.state)
+    console.log(event.target.className);
+    if(event.target.className === "GoodBtn") {
+        this.setState(prevState => ({
+            good: prevState.good + 1,
+        }))
+    }
+    if(event.target.classList.contains("NeutralBtn")) {
+        this.setState(prevState => ({
+            neutral: prevState.neutral + 1,
+        }))
+    }
+    if(event.target.className === "BadBtn") {
+        this.setState(prevState => ({
+            bad: prevState.bad + 1,
+        }))
+    }
+}
+
 onGoodVote = () => {
     this.setState(prevState => ({
         good: prevState.good + 1,
@@ -25,30 +47,51 @@ onBadVote = () => {
     }))
 }
 
+countTotalFeedback = (a, b, c) => {
+    const totalFeedbackAmount = a + b +c;
+    return totalFeedbackAmount;
+}
+
+countPositiveFeedbackPercentage = (good, neutral, bad) => {
+    if(good > 0 || neutral > 0 || bad > 0) {
+        const totalFeedbackAmount = good + neutral + bad;
+        const totalPositive = (good * 100) / totalFeedbackAmount;
+        return `${Math.round(totalPositive)}%`;
+    } else {
+        return "no stats data yet"
+    }
+    
+}
 
     render() {
         return (
-            <>
+            <Wrapper>
             <BtnSection>
-                <ButtonOne onClick={this.onGoodVote}>Good</ButtonOne>
-                <ButtonTwo onClick={this.onNeutralVote}>Neutral</ButtonTwo>
-                <ButtonThree onClick={this.onBadVote}>Bad</ButtonThree>
+                <button className='GoodBtn' onClick={this.onLeaveFeedback}>Good</button>
+                <button className='NeutralBtn' onClick={this.onLeaveFeedback}>Neutral</button>
+                <button className='BadBtn' onClick={this.onLeaveFeedback}>Bad</button>
             </BtnSection>  
             
             <StatisticsSection>
                     <StatisticsH2>Statistics</StatisticsH2>
-                    <div>Good:{this.state.good}</div>
-                    <div>Neutral:{this.state.neutral}</div>
-                    <div>Bad:{this.state.bad}</div>
+                        <div>Good:{this.state.good}</div>
+                        <div>Neutral: {this.state.neutral}</div>
+                        <div>Bad: {this.state.bad}</div>
+                        <div>Total: {this.countTotalFeedback(this.state.good, this.state.neutral, this.state.bad)}</div>
+                        <div>Positive feedback: {this.countPositiveFeedbackPercentage(this.state.good, this.state.neutral, this.state.bad)}</div>
             </StatisticsSection>
-            </>
+            </Wrapper>
         )
     }
 }
 
 export default Statistics
 
-
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+`
 
 const BtnSection = styled.div`
         width: 350px;
@@ -66,27 +109,27 @@ const StatisticsH2 = styled.h2`
     text-align: center;
 `
 
-const ButtonOne = styled.button`
-    background-color: white;
-    width: 80px;
-    height: 20px;
-    outline: none;
-    border: 1px solid yellow;
-    border-radius: 3px;
-`
-const ButtonTwo = styled.button`
-    background-color: white;
-    width: 80px;
-    height: 20px;
-    border: 1px solid yellow;
-    height: 20px;
-    border-radius: 3px;
-`
-const ButtonThree = styled.button`
-    background-color: white;
-    width: 80px;
-    height: 20px;
-    outline: none;
-    border: 1px solid yellow;
-    border-radius: 3px;
-`
+// const GoodBtn = styled.button`
+//     background-color: white;
+//     width: 80px;
+//     height: 20px;
+//     outline: none;
+//     border: 1px solid yellow;
+//     border-radius: 3px;
+// `
+// const NeutralBtn = styled.button`
+//     background-color: white;
+//     width: 80px;
+//     height: 20px;
+//     border: 1px solid yellow;
+//     height: 20px;
+//     border-radius: 3px;
+// `
+// const BadButton = styled.button`
+//     background-color: white;
+//     width: 80px;
+//     height: 20px;
+//     outline: none;
+//     border: 1px solid yellow;
+//     border-radius: 3px;
+// `
